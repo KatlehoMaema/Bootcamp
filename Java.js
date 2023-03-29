d15729085a9d495e50d19333d1802b6463e27c0f
Kgotso Maema <lolwethu0988@mail.com>
2023-03-29 16:09:27 +0200

    push javascript fundamentals

---
+++ Java.js
@@ -0,0 +1,35 @@
+//var a=1;
+//var a=10;
+//let a=1;
+//let a=10;
+//let a=1;
+//let b=2;
+//let c=a+b;
+//let a="10"
+//let b=10;
+//if (a==b){
+  //  console.log("true");
+//else {
+  //  console.log("false");
+//let a=10;
+//let b=20;
+//let c=10;
+//let a=20;
+//if (a<20){
+    //console.log ("Greater than a decade");
+//} else if (a==10){
+    //console.log ("it is a decade");
+//} else if ("a<10||a>0") {
+    //console.log ("it is lesser than a decade");
+//} else {
+    //console.log("we cannot have negetive years!")
+let a= 30;
+let b= 17;
+let c= 13;
+if (a>b && a>c){
+    console.log(a);
+} else if (b>a && b>c) {
+    console.log(b)
+} else if (c>a && c>b) {
+
+}
