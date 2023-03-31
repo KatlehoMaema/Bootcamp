66ce044c7cde35b4cc3c5fc10c5ebf353c04585c
Kgotso Maema <lolwethu0988@mail.com>
2023-03-31 16:10:14 +0200

    Array

---
+++ MyCreation/Java.js
@@ -0,0 +1,91 @@
+//var a=1;
+//var a=10;
+//let a=1;
+//let a=10;
+//let a=1;
+//let b=2;
+//let c=a+b;
+//let a="10"
+//let b=10;
+//if (a==b){
+  //  console.log("true");
+//else {
+  //  console.log("false");
+//let a=10;
+//let b=20;
+//let c=10;
+//let a=20;
+//if (a<20){
+    //console.log ("Greater than a decade");
+//} else if (a==10){
+    //console.log ("it is a decade");
+//} else if ("a<10||a>0") {
+    //console.log ("it is lesser than a decade");
+//} else {
+    //console.log("we cannot have negetive years!")
+//let a= 30;
+//let b= 17;
+//let c= 13;
+//if (a>b && a>c){
+    //console.log(a);
+//} else if (b>a && b>
+    //c) {
+    //console.log(b)
+//} else if (c>a && c>b) {
+
+//check if the number is even
+// let z= 19
+// let y= 2
+// let x= 5 % 2
+// console.log (x);
+
+
+// //function checkEvenNumber(){
+//     //let a = document.getElementById("evennumber").value
+
+//     //if (a % 2 == 0) {
+//     //   alert(a + "it is an even number") 
+//     // else {
+//       //  alert(a + "odd number") 
+//     //}
+
+// //For loop
+
+// for (let i = 0; i < 9; i++) {
+//     console.log(i)
+// }
+
+// for (let i = 0; i <= 9; i++) {
+//     console.log(i)
+// }
+//Find even numbers
+
+// let numbers = [2,9,11,7,5,3,4,8,15,21,19]
+    
+// for(let k = 0; k <= numbers.length; k++){
+//    if(numbers[k
+// ] % 2 == 0){
+//     console.log(numbers[k])
+
+//OR in order for it not give me an Iedntified statement I shall do this
+
+//for(let k=0; k< numbers.length; k++){
+    // if(numbers[k] % 2==0){
+    //     console.log(numbers[k])
+    // }
+
+//}
+
+//let fnames= ["kgotso","luks","vuyo","ncesh",]
+
+//for (k = 0; k < fnames.length; k++) {
+//    if(fnames[k])
+//    console.log(fnames[k][0])
+//}
+
+let fullname= [kaygee]
+
+for (k=0; k < fullname.length; k++) {
+    if(fullname[k])
+    console.log()
+}
