7adfede1ffa07f145935a3de7c89fa86fe277e6f
Kgotso Maema <lolwethu0988@mail.com>
2023-04-19 11:39:15 +0200

    new updates

--- ImageViewer/image.js
+++ ImageViewer/image.js
@@ -1,30 +1,39 @@
 //const gallery = document.getElementById ('gallery');
 //const viewer = new viewer(gallery);
+
+
 let show = document.querySelector(".image-slide")
 
-let image = [
-   "image1.jpg",
-   "image2.jpg",
-   "image3.jpg",
-   "image4.jpg",
-   "image6.jpg",
-   "image7.jpg",
-   "image8.jpg",
-]
+
+ let image = [
+    "image1.jpg",
+    "image2.jpg",
+    "image3.jpg",
+    "image4.jpg",
+    "image6.jpg",
+    "image7.jpg",
+    "image8.jpg",
+ ]
 
 let current = 0;
 function save() {
     let img = document.querySelector(".file").value;
     image.push(img);
     console.log(img)
-    document.querySelector("file").value = '';
-}
 
+    //
+//    document.querySelector("file").value = '';
+}
+//adding a picture using add btn
+
+
+
+//Creating the next and prev btn
 function next(){
     
-   current++
+    current++
     if(current >= image.length){
-        current = 0
+        current = 0;
 
     }
     show.src = image[current]
@@ -39,22 +48,43 @@ function prev(){
     show.src = image[current]
 }
 
-let pictures = [
-    "image1.jpg",
-    "image2.jpg",
-    "image3.jpg",
-    "image4.jpg",
-    "image6.jpg",
-    "image7.jpg",
-    "image8.jpg",
-]
- //localStorage.setItem("image",JSON.stringfy(pictures))
-localStorage.setItem("pictures", JSON.stringify(image));
-localStorage.getItem(image);
+function removeImage(){
 
-
-let mages;
-if(localStorage.getItem("images")){
-    image = JSON.parse(localStorage.getItem("images"));
-
+    image.splice (current,1);
 }
+
+function add(){
+    let image = document.querySelector(".new-image").value;
+
+//console.log(image);
+
+//    console.push(image);
+    localStorage.setItem("pictures", JSON.stringify(image));
+}
+
+// let pictures = [
+//     "image1.jpg",
+//     "image2.jpg",
+//     "image3.jpg",
+//     "image4.jpg",
+//     "image6.jpg",
+//     "image7.jpg",
+//     "image8.jpg",
+// ]
+image.push(img)
+ localStorage.setItem("pictures", JSON.stringify(image))
+ localStorage.getItem(image);
+
+//function removeImage(){
+//    image.splice(i,1);
+//}
+
+//  let images;
+//  if(localStorage.getItem("keygee")){
+//      image = JSON.parse(localStorage.getItem("keygee"));
+//  }else{
+//      //image["image1.jpg","image2","image3.jpg","image4.jpg","image6.jpg","image7.jpg","image7.jpg"];
+//  }
+
+
+
